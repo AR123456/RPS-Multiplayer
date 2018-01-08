e0116ab45d9205036a17e1b341b56546144135bb
Anne <coloradoz99@gmail.com>
2018-01-07 19:43:27 -0700

    updates to game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,13 +39,16 @@ $(document).ready(function(){
     // Get a reference to the database service
     var database = firebase.database();
       //avalible game choices 
+      // Creating variables to hold the number of wins, losses, and ties. They start at 0.
     var guess = ["rock","paper","scissors"];
     var name ="";
-    var message ="";
+    var chat ="";
     var wins = 0;
     var losses = 0;
     var ties = 0;
-    //settting up player object to match the database 
+    var turn =1;
+
+    //setting up player object to match the database 
     var Player1 = {
         name: "",
         guess:"",
@@ -56,56 +59,129 @@ $(document).ready(function(){
         }
     var Player2 = {
         name: "",
-        gameAction:"",
+        guess:"",
         wins:0,
         losses:0,
         ties:0,
         chat:""
         } 
-    
-    //so far this is not working 
-            // var chats = database.ref('chat');
+    // initial load starting game , compare value of on click to database to determine if player one or player two 
+  // Capture Button Click to add players
+  $("#add-player").on("click", function() {
+    // get  player for Firebase .
+        name = $("#name-input").val().trim();
+        console.log(name);
 
-            //       // On-click function for submitting a chat.
-            //       $('#chat').on('click', function () {
-            //         var message = $('#message');
-            //         var chatObj = {
-            //             message: message.val(),
-            //             timestamp: firebase.database.ServerValue.TIMESTAMP,
-            //             sender: player.name
-            //         };
-            //         chat.push(chatObj);
+    database.ref().on("value",function(snapshot){
+        if (snapshot.child("Player1").exists()){
 
-            //         // Clear message input.
-            //         message.val('');
+//  make Player2 this name - send to database 
+           database.ref().set({
+            name: Player2.name,
+  
+           })
+//write player2 name to html
+           $("#playerTwo").html(Player2.name);
 
-            //         return false;
-            //     });
+//and execute the  playGame function 
+            playGame();//play game function 
+            
+        } else{
+           // make this name Player 1 - send to database
+           database.ref().set({
+            name: Player1.name,
+  
+           })
 
-            //     // Database listening function for chats.
-            //     chats.on('child_added', function (snapshot) {
-            //         if (snapshot.val()) {
-            //             DOMFunctions.showChats(snapshot);
-            //         }
-            //     });
+           // write it to HTML 
+           $("#playerOne").html(Player1.name);
+
+        }
 
 
-    
+     // Don't refresh the page 
+      event.preventDefault();
 
-   // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+    });// end of evaluating for player 
+
+    function playGame(){
  
-  //events  store them in an object that matches the format of the firebase database 
-    //need click event for r,p,s button click will be same event for all three speciffiy in the evert 
+        if (turn = 1) {
+        //  For player one just below rock paper scissors show Hi ____name You are player 1 next line its your turn
+         // For player 2 show Hi ____name You are player 2 next line waiting for ___ to choose
+             // in player1 box area show rock,paper,scissor in HTML as buttons 
+             //on click event selects rock , paper or scissor 
+             // sends choice to dbB
+            // shows choice in player one area
+            //shows waiting for ---name of player 2
+            // changes player 2 area to its your turn 
+            turn++;
+             } else {
+                // in player1 box area show rock,paper,scissor in HTML as buttons 
+                 //on click event selects rock , paper or scissor 
+                 // sends choice to dbB
+                 // shows choice in player two area
+             }
+             evaluateWinner();
+              //run function to eval winner, 
+            }
+            
+     function evaluateWinner(){
+          // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+      if ((player2.guess === “rock”) || (player1.guess === “paper”) || (player1.guess === “scissors”)) {
+
+        if ((player1.guess === “rock”) && (player2.guess === “scissors”)) {
+          wins++;//player1
+          losses++;//player2
+          //update DB 
+          //update HTML winner, new scores 
+        } else if ((player1.guess === “rock”) && (player2.guess === “paper”)) {
+          losses++;//player1
+          wins++;//player2
+              //update DB 
+          //update HTML winner, new scores 
+        } else if ((player1.guess === “scissors”) && (player2.guess === “rock”)) {
+          losses++;//player1
+          wins++;//player2
+              //update DB 
+          //update HTML winner, new scores 
+        } else if ((player1.guess === “scissors”) && (player2.guess === “paper”)) {
+          wins++;//player1
+          losses++;//player2
+           //update DB 
+          //update HTML winner, new scores 
+        } else if ((player1.guess === “paper”) && (player2.guess === “rock”)) {
+          wins++;//player1
+          losses++;//player2
+              //update DB 
+          //update HTML winner, new scores 
+        } else if ((player1.guess === “paper”) && (player2.guess === “scissors”)) {
+          losses++;//player1
+          wins++;//player2
+              //update DB 
+          //update HTML winner, new scores 
+        } else if (player1.guess === player2.guess) {
+          ties++;//both players 
+              //update DB 
+          //update HTML winner, new scores 
+        }
+       turn=1; 
+        }   }     
+    
+   //function to evaluate for winner pass in the guesses as peramitners , get parm from FB
+  //need click event for r,p,s button click will be same event for all three speciffiy in the evert 
     //in this click event update firebase database ,change the playerview in HTML 
     //if the game is over
-      //run function to eval winner, 
-      //end game function call
-
-  //custom functions
+        //end game function call
+  
+  //events  store them in an object that matches the format of the firebase database 
+    //custom functions
       //function is game over
-      //function to evaluate for winner pass in the guesses as peramitners , get parm from FB
-
-      //funciton end game
+      function gameOver(){
+            //a player leave game put disconnect message in chat
+           // make remaining player player one
+            // if another player joins re start game 
+       }; 
         //show final views and what video says to do next
     // chat message functions -add message   
 
